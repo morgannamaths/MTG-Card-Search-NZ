da6ee7fa1dc0b1cecb216501e2cf7521a002514b
morgy <122200270+themathsgirl@users.noreply.github.com>
2023-11-23 11:51:50 +1300

    Add files via upload

---
+++ MTG Card Search NZ.py
@@ -0,0 +1,41 @@
+import time
+import pyautogui as pg
+
+time.sleep(2)
+search = open("MTG Cards To Search.txt", "r").readlines()
+
+sites = ["shuffleandcutgames", "gamingdna", "cardmerchant", "goblingames"]
+website = sites[0]
+
+i = 0
+for line in search:
+    search[i] = line.replace(" ", "+")
+    i += 1
+
+# shuffleandcutgames
+for i in range(len(search)):
+    if website == "shuffleandcutgames":
+        if i == 0:
+            pg.write("firefox ")
+        # .strip() removes \n at end of line
+        pg.write(f"https://www.shuffleandcutgames.co.nz/search?q=*{search[i].strip()}* ")
+
+    if website == "gamingdna":
+        if i == 0:
+            pg.write("firefox ")
+        # .strip() removes \n at end of line
+        pg.write(f"https://gamingdna.co.nz/search?q=*{search[i].strip()}* ")
+
+    if website == "cardmerchant":
+        if i == 0:
+            pg.write("firefox ")
+        # .strip() removes \n at end of line
+        pg.write(f"https://cardmerchant.co.nz/search?q=*{search[i].strip()}* ")
+
+    if website == "goblingames":
+        if i == 0:
+            pg.write("firefox ")
+        # .strip() removes \n at end of line
+        pg.write(f"https://goblingames.nz/search?q={search[i].strip()} ")
+
+pg.press("enter")
